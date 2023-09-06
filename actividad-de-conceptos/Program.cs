d121f37753df0a3cc978dc297160417c1f72f184
Nataly12cg <natalycorrea99@gmail.com>
2023-09-06 13:55:14 -0500

    Agregado ejercicio Positive Power

---
+++ actividad-de-conceptos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PositivePower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresa un número: ");
+            if (double.TryParse(Console.ReadLine(), out double numero))
+            {
+                if (numero > 0)
+                {
+                    double resultado = Math.Pow(numero, 2);
+                    Console.WriteLine($"Resultado: {resultado}");
+                }
+                else if (numero < 0)
+                {
+                    Console.WriteLine("Resultado: Número negativo.");
+                }
+                else
+                {
+                    Console.WriteLine("Resultado: 0");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entrada no válida. Ingresa un número válido.");
+            }
+        }
+    }
+}
+
